961f13f9c0e3a66f39f82a80dadbc47cc0a41f10
tyron <74818961+tyron12233@users.noreply.github.com>
2025-11-10 17:36:44 +0000

    feat: implement default routes and health check

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'Restaurant LLM API is running!'
+    });
+});
+
+app.use((req: Request, res: Response) => {
+    res.status(404).json({
+        message: 'Route not found'
+    })
+})
 
 app.use(errorHandler)
 
