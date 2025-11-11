156e92f8b4ba197cd51dfef44758d0ae5faafdd2
tyron <74818961+tyron12233@users.noreply.github.com>
2025-11-11 13:41:51 +0000

    refactor: add failsafe for schema parsing

--- src/integrations/gemini.client.ts
+++ src/integrations/gemini.client.ts
@@ -4,34 +4,47 @@ import { GeminiRestaurantQueryResponse } from "../types/gemini.types";
 import { querySchema as geminiQuerySchema } from "../types/gemini.schema";
 import { config } from "../utils/config";
 
+const SYSTEM_INSTRUCTION = 'You are an assitant that extracts search ' + 
+'parameters for finding restaurants from a user message.'
+
 const googleAi = new GoogleGenAI({
   apiKey: config.GEMINI_API_KEY,
 });
+
 export const geminiClient = {
   /**
-   *
+   * Processes the raw string using the Gemini API.
+   * This uses the responseJsonSchema parameter consumed by Gemini
+   * to make sure that the response adheres to what the Foursquare API.
+   * 
    * @param message The message to be processed
    * @returns A promise containing the parsed response from the message
    */
   async processMessage(
     message: string
-  ): Promise<GeminiRestaurantQueryResponse | null> {
+  ): Promise<GeminiRestaurantQueryResponse> {
     const response = await googleAi.models.generateContent({
       model: "gemini-2.5-flash",
       config: {
         responseMimeType: "application/json",
         responseJsonSchema: zodToJsonSchema(geminiQuerySchema),
-        systemInstruction:
-          "You are an assistant that extracts search parameters for finding restaurants from user messages. Respond only with a JSON object matching the specified schema. Provide near if a location is specified in the message.",
+        systemInstruction: SYSTEM_INSTRUCTION
       },
       contents: {
-        text:
-          "Extract restaurant search parameters from the following message: " +
-          message,
+        text: message,
       },
     });
 
-    console.log("Gemini Response:", response.text);
-    return geminiQuerySchema.parse(JSON.parse(response.text ?? ""));
+    if (!response.text) {
+      throw new Error("Gemini API did not return any result.")
+    }
+
+    const safeParse = geminiQuerySchema.safeParse(JSON.parse(response.text));
+
+    if (!safeParse.success) {
+      throw new Error("INTERNAL ERROR: Gemini returned an output that does not adhere to the schema: " + safeParse.error.format())
+    }
+
+    return safeParse.data as GeminiRestaurantQueryResponse;
   },
 };
--- src/services/restaurant.service.ts
+++ src/services/restaurant.service.ts
@@ -5,14 +5,19 @@ import {
 import { geminiClient } from "../integrations/gemini.client";
 
 export const restaurantService = {
+
+  /**
+   * Processes the raw message string.
+   * 
+   * Message is processed through gemini, which is then passed 
+   * to the foursquare api.
+   */
   async processMessage(message: string): Promise<FoursquareRestuarant[]> {
     const llmQuery = await geminiClient.processMessage(message);
 
     if (!llmQuery) {
       throw new Error("LLM: Failed to extract query parameters from message");
     }
-
-    console.log("LLM Extracted Query:", llmQuery);
 
     const results = await foursquareClient.searchRestuarants(llmQuery);
     return results.results;
